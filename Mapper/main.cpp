51b72614b229b19c0589f00229b0c8e8af8224b0
osixan <osixan@osixan-HP-ENVY-15-Notebook-PC>
2015-12-05 17:50:29 +0100

    adding some code does not work doe because out of bound with .at function with int end

--- Mapper/main.cpp
+++ Mapper/main.cpp
@@ -13,30 +13,67 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <string.h>
+#include <stdio.h>
+
+#define MAXLENGTH 320
 
 using namespace std;
+string map;
 
 /*
  * 
  */
 
-char* mapper()
-{
-    return NULL;
+void next_word( string* line ) {
+    
+    int end = 0;
+    line->at(0);
+    while( line->at(0) ==  ' ' || line->at(0) == '\n' || line->at(0) == '\t') *line = line->substr(1,line->size());
+    while( line->at(end) !=  ' ' || line->at(end) != '\n' || line->at(end) != '\t') ++end;
+    map.append( line->substr(0,end-1) + '\n' );
+    *line = line->substr(end,line->size());
+    
 }
 
-char* import_file(char* filename)
+void mapper( string* line )
 {
-    return NULL;
+
+    while(!line->empty()) next_word(line);
+    
+}
+
+int import_file(string filename, string* lines)
+{
+    ifstream file;
+    file.open(filename.c_str());
+    int a = 0;
+    if(file.is_open())
+    {
+        while( getline(file,lines[a]) )
+        {
+            ++a;
+        }
+        file.close();
+    }
+    else cout << "Could Not Open File";
+    return a;
 }
 
 int main(int argc, char** argv) {
-    cout << "Hello";
     
-    //import file IDIOT maybe seperate function
+    string* lines = new string[MAXLENGTH];
+    int b = import_file("test.txt",&lines[0]);
     
-    
+    //DONE import file IDIOT maybe seperate function
+    int a = 0;
+    while( a < 1 ) {
+        mapper(&lines[a]);
+    }
     
     return 0;
+    
+    cout << map;
 }
 
